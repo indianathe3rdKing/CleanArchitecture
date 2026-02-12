079076e573fa458aae10e0325b88222c7904310f
indianathe3rd <indiemenziwa@gmail.com>
2026-02-12 15:18:45 +0200

    data

---
+++ app/src/main/java/com/example/cleanarchitecturetest/MainActivity.kt
@@ -0,0 +1,101 @@
+package com.example.cleanarchitecturetest
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.cleanarchitecturetest.ui.theme.CleanArchitectureTestTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CleanArchitectureTestTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(viewModel: NotesViewModel, modifier: Modifier = Modifier) {
+    var text by remember { mutableStateOf("") }
+    LaunchedEffect(Unit) { viewModel.loadNotes }
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            OutlinedTextField(
+                value = text,
+                onValueChange = { text = it },
+                label = { Text("Enter a note") },
+                modifier = Modifier.weight(1f)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Button(onClick = {
+                viewModel.addNote(text)
+                text = ""
+            }) {
+                Text("Add")
+
+            }
+        }
+        LazyColumn {
+            items(viewModel.notes) { notes ->
+                Row(
+                    modifier= Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 5.dp),
+                    verticalAlignment = Alignment.CenterVertically
+
+                ) {
+                    Text(notes.text, modifier = Modifier.weight(1f))
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Button(onClick = {viewModel.deleteNote(notes)}) {
+                        Text("Delete")
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CleanArchitectureTestTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/example/cleanarchitecturetest/data/Note.kt
@@ -0,0 +1,6 @@
+package com.example.cleanarchitecturetest.data
+
+data class Note(
+    val id: Int,
+    val text: String
+)
